36db7a60a3dfe59ca92a70a96b144e429dbc77c0
Xuan My <nnxmy97@gmail.com>
2018-11-26 17:05:54 +0700

    reset nums of right/wrong answers and score

--- controllers/HomePage.js
+++ controllers/HomePage.js
@@ -12,6 +12,9 @@ router.post('/', (req, res) => {
     req.session.field = req.body.inputField;
     req.session.difficulty = req.body.inputDifficulty;
     req.session.time = 60;
+    req.session.right = 0;
+    req.session.wrong = 0;
+    req.session.score = 0;
     res.redirect(req.body.url);
 });
 
